8a9fe5688e1ee584e84024bed56d82f064a18032
zxckv <zack.langley@icloud.com>
2024-09-23 19:38:03 +0100

    Updated main to v2

--- app/nav.js
+++
@@ -1,8 +0,0 @@
-// $(".navigation-mobile").click( function() { $('.navigation-mobile-dropdown-before').toggleClass('navigation-mobile-dropdown-after'); });
-
-let down = false;
-document.getElementById("navigation-mobile").addEventListener("click", () => {
-    let elem = document.getElementById("navigation-mobile-dropdown");
-    elem.classList.toggle("dropdown-before");
-    elem.classList.toggle("dropdown-after");
-});
